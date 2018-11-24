0733e98102243d4ffd766c2af1928ada8135e40c
XU Xiaohchuan <35758677+incandescentxxc@users.noreply.github.com>
2018-11-24 17:16:52 +0800

    Doc.update

--- FINALPROJECT/src/Piece.java
+++ FINALPROJECT/src/Piece.java
@@ -2,65 +2,68 @@
  * This class is about different kinds of animals in
  * the gameboard.
  */
-public class Piece{
+public class Piece {
     /**
      * rank refers to the relation indicating whether an animal
      * can eat another animal
      */
     protected int rank;
     /**
-     * true for in water, false for not in water(by defalut)
-     */
-    public boolean isInWater = false;
-
-
-    /**
      * side refers to the user an animal belongs to
      * 0 for one user, 1 for another user
      */
     private final int side; // 0 or 1
-
     /**
      * the name of this piece
      */
     private final String name;
-
+    /**
+     * true for in water, false for not in water(by defalut)
+     */
+    public boolean isInWater = false;
+    /**
+     * a piece know itself whether itself is in a trap.
+     * True for in trap,vice versa.
+     */
     protected boolean isInTrap = false;
+
     /**
      * Constructor
+     *
      * @param side this represents the user that the pieces belong to
      * @param name the name of pieces
      */
-    Piece( int side, String name) {
+    Piece(int side, String name) {
         this.side = side;
         this.name = name;
     }
 
-
     /**
      * gets which user the piece belongs to
+     *
      * @return an integer indicating the user that piece belongs to
      */
-
-    public int getSide(){
+    public int getSide() {
         return side;
     }
 
     /**
-     *
      * @return the dynamic rank value (after instantiation )
      */
-    public int getRank(){return  rank ;}
+    public int getRank() {
+        return rank;
+    }
 
     /**
      * @return the name of this piece
      */
-    public String getName(){return name;}
-
-
+    public String getName() {
+        return name;
+    }
 
     /**
      * judges whether two pieces belongs to one user
+     *
      * @param other the other piece
      * @return boolean value(if two pieces from one user, true
      * vice versa )
@@ -71,24 +74,15 @@ public class Piece{
 
     /**
      * general method to judge whether this piece can eat another
+     *
      * @param other another piece
      * @return true for being able to eat,vice versa
      */
-    public boolean ableToCapture(Piece other){
+    public boolean ableToCapture(Piece other) {
         boolean ability = false;
-        if(this.isSameSide(other)) ability = false;
+        if (this.isSameSide(other)) ability = false;
         else if (this.getRank() >= other.getRank() || other.isInTrap) ability = true;
         return ability;
 
     }
-
-
-
-
-
-
-
-
-
-
 }
